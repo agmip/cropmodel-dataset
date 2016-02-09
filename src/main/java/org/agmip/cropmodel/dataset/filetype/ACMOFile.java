5e33a8e81b6d6288f90f5f0a1ea113234be767ce
Christopher Villalobos <cvillalobos@ufl.edu>
2016-02-09 10:54:44 -0500

    Implemented ACMO format checking
    * Checks for mismatched header/data columns
    * Makes sure dates are in ISO format
    * Implemented tests for above

--- src/main/java/org/agmip/cropmodel/dataset/filetype/ACMOFile.java
+++ src/main/java/org/agmip/cropmodel/dataset/filetype/ACMOFile.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2012-2016, AgMIP All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the AgMIP nor the names of its
  *   contributors may be used to endorse or promote products derived from this
  *   software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,21 +28,161 @@
  */
 package org.agmip.cropmodel.dataset.filetype;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
-/**
- *
- * @author Christopher Villalobos <cvillalobos@ufl.edu>
- */
+import com.opencsv.CSVReader;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ACMOFile extends CropModelFile {
+  private static final Logger LOG = LoggerFactory.getLogger(ACMOFile.class);
+  private static final DateTimeFormatter DATE_FORMAT = ISODateTimeFormat.date();
+  private Optional<String[]> header;
 
-    public ACMOFile(Path path) {
-        super(path);
-    }
+  public ACMOFile(Path path) {
+    super(path);
+    loadHeader();
+  }
 
-    @Override
-    public CropModelFileType getFileType() {
-        return CropModelFileType.ACMO;
+  @Override
+  public CropModelFileType getFileType() {
+    return CropModelFileType.ACMO;
+  }
+
+  @Override
+  public boolean isValid() {
+    if (header.isPresent()) {
+      return checkFormat();
+    } else {
+      return false;
     }
-    
+  }
+
+  public Optional<String[]> getHeader() {
+    return this.header;
+  }
+
+  public boolean checkFormat() {
+
+    boolean errors = false;
+    if (this.header.isPresent()) {
+      int headerLength = this.header.get().length;
+      List<Integer> dateColumns = getDateColumns();
+      try (CSVReader reader = new CSVReader(new FileReader(this.path.toFile()))) {
+        Optional<String[]> nextLine = Optional.ofNullable(reader.readNext());
+        long lineNum = 0L;
+        while(nextLine.isPresent()) {
+          boolean lineError = false;
+          boolean dateError = false;
+          lineNum++;
+          String[] line = nextLine.get();
+          if (line.length != headerLength) {
+            lineError = true;
+            errors = true;
+            LOG.error("Invalid number of columns on line " + lineNum);
+          }
+
+          if (! lineError ) {
+            if (! line[0].equals("")) {
+              char token = (line[0].startsWith("\"")) ? line[0].charAt(1) : line[0].charAt(0);
+              if (token == '*') {
+                StringBuffer errorLines = new StringBuffer("Invalid date for ");
+                int errorsFound = 0;
+                for(Integer idx : dateColumns) {
+                  if (! line[idx].equals("")) {
+                    try {
+                      LocalDate d = DATE_FORMAT.parseLocalDate(line[idx]);
+                    } catch (IllegalArgumentException ex) {
+                      errorsFound ++;
+                      errorLines.append(this.header.get()[idx]);
+                      errorLines.append(", ");
+                      dateError = true;
+                    }
+                  }
+                }
+                if (dateError) {
+                  errors = true;
+                  errorLines.deleteCharAt(errorLines.lastIndexOf(","));
+                  if (errorsFound > 1) {
+                    errorLines.insert(12, 's');
+                    errorLines.insert(errorLines.lastIndexOf(",")+1, " and");
+                    if (errorsFound == 2) {
+                      errorLines.deleteCharAt(errorLines.lastIndexOf(","));
+                    }
+                  }
+                  errorLines.append("on line ");
+                  errorLines.append(lineNum);
+                  LOG.error(errorLines.toString());
+                  errorLines = null;
+                }
+              }
+            }
+          }
+          nextLine = Optional.ofNullable(reader.readNext());
+        }
+      } catch (IOException ex) {
+        return false;
+      }
+    } else {
+      // The format is incorrect if it has no header
+      errors = true;
+    }
+    return !errors;
+  }
+
+  private void loadHeader() {
+    loadHeader(false);
+  }
+
+  private void loadHeader(boolean recheck) {
+    if (recheck || null == this.header) {
+      try (CSVReader reader = new CSVReader(new FileReader(this.path.toFile()))) {
+        Optional<String[]> nextLine = Optional.ofNullable(reader.readNext());
+        long lineNum = 0L;
+        while(nextLine.isPresent()) {
+          lineNum++;
+          String[] line = nextLine.get();
+          if (! line[0].equals("")) {
+            char token = (line[0].startsWith("\"")) ? line[0].charAt(1) : line[0].charAt(0);
+            if (token == '#') {
+              LOG.debug("Header found on " + lineNum);
+              this.header = nextLine;
+              break;
+            }
+          }
+          nextLine = Optional.ofNullable(reader.readNext());
+        }
+      } catch (IOException ex) {
+        this.header = Optional.empty();
+      }
+      if (null == this.header) {
+        LOG.error("No header found in file  " + this.path.toString());
+        this.header = Optional.empty();
+      }
+    }
+  }
+
+  private List<Integer> getDateColumns() {
+    List<Integer> colNum = new ArrayList<>();
+    if (header.isPresent()) {
+      String[] h = header.get();
+      int l = h.length;
+      for(int i=0; i < l; i++) {
+        if (h[i].endsWith("DAT") || h[i].endsWith("DATE") || h[i].endsWith("DAT_S")) {
+          colNum.add(i);
+        }
+      }
+    }
+    return colNum;
+  }
 }
---
+++ src/test/java/org/agmip/cropmodel/dataset/filetype/ACMOFileTest.java
@@ -0,0 +1,95 @@
+package org.agmip.cropmodel.dataset.filetype;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+public class ACMOFileTest {
+  @BeforeClass
+  public static void setupClass() {
+    cleanAcmo = loadResource("clean.acmo");
+    headerlessAcmo = loadResource("headerless.acmo");
+    dateproblemAcmo = loadResource("dateissue.acmo");
+    corruptedAcmo = loadResource("corrupted.acmo");
+    blanklinesAcmo = loadResource("blanklines.acmo");
+    apsim1230Acmo = loadResource("apsimerror.acmo");
+  }
+
+  private static Optional<ACMOFile> loadResource(String file) {
+    try {
+    URL url = ACMOFileTest.class.getResource(file);
+    if (url != null) {
+      return Optional.ofNullable(new ACMOFile(Paths.get(url.toURI())));
+    } else {
+      return Optional.empty();
+    }
+    } catch (URISyntaxException ex) {
+      ex.printStackTrace();
+      return Optional.empty();
+    }
+  }
+
+  private static Optional<ACMOFile> cleanAcmo;
+  private static Optional<ACMOFile> headerlessAcmo;
+  private static Optional<ACMOFile> dateproblemAcmo;
+  private static Optional<ACMOFile> corruptedAcmo;
+  private static Optional<ACMOFile> blanklinesAcmo;
+  private static Optional<ACMOFile> apsim1230Acmo;
+
+  @Test
+  public void testCleanHeaderPresent() {
+    if (cleanAcmo.isPresent()) {
+      assertNotNull(cleanAcmo.get().getHeader());
+      assertTrue(cleanAcmo.get().getHeader().isPresent());
+    } else {
+      fail("Cannot find clean ACMO file");
+    }
+  }
+
+  @Test
+  public void testHeaderlessHeaderPresent() {
+    if (headerlessAcmo.isPresent()) {
+      assertNotNull(headerlessAcmo.get().getHeader());
+      assertFalse(headerlessAcmo.get().getHeader().isPresent());
+    } else {
+      fail("Cannot find headerless ACMO file");
+    }
+  }
+
+  @Test
+  public void testCleanFormatValid() {
+    if (cleanAcmo.isPresent()) {
+      assertTrue("Found errors in clean ACMO", cleanAcmo.get().checkFormat());
+    } else {
+      fail("Cannot find clean ACMO file");
+    }
+  }
+
+  @Test
+  public void testHeaderlessFormatValid() {
+    if (headerlessAcmo.isPresent()) {
+      assertFalse("No errors found in headerless", headerlessAcmo.get().checkFormat());
+    } else {
+      fail("Cannot find headerless ACMO file");
+    }
+  }
+
+  @Test
+  public void testDateProblemValid() {
+    if (dateproblemAcmo.isPresent()) {
+      assertFalse("No errors found in dateissue", dateproblemAcmo.get().checkFormat());
+    } else {
+      fail("Cannot find dateissues ACMO file");
+    }
+  }
+}
